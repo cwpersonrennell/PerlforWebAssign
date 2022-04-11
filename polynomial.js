1dc60bf871836b7a343e138118368280a03b4e4c
cwpersonrennell <94017839+cwpersonrennell@users.noreply.github.com>
2022-04-11 13:50:17 -0700

    Update polynomial.js

--- polynomial.js
+++ polynomial.js
@@ -22,7 +22,7 @@ class ComplexN{
   pow(n){
     let result = new ComplexN(1,0);
     for(let i = 0;i<n;i++){
-      result = result.multiply(result);
+      result = result.multiply(this);
     }
     return result;
   }
