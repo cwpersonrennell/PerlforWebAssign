33902f384b1775b236bcb1e6fe9dc46c480efb7b
cwpersonrennell <94017839+cwpersonrennell@users.noreply.github.com>
2022-04-11 13:01:19 -0700

    Update polynomial.js

--- polynomial.js
+++ polynomial.js
@@ -1,1 +1,10 @@
-
+class ComplexN{
+  constructor(re,im){
+  this.re = re;
+  this.im = im;
+  }
+  
+  multiply(other){
+    return new ComplexN(this.re*other.re-this.im*other.im,this.re*other.im+this.im*other.im);
+  }
+}
